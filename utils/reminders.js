87cccb94d7f03d12b4724bc64ec07245a0d4c47d
tmurvv <tmurv@shaw.ca>
2021-07-17 09:30:42 -0600

    testing schedule

--- utils/reminders.js
+++ utils/reminders.js
@@ -16,6 +16,7 @@ const {Users} = require('../schemas/UserSchema');
             let workdayMillies = 3*60*60*24*1000;
             if (new Date().getDay()===1) workdayMillies=5*60*60*24*1000;
             if (new Date().getDay()===2) workdayMillies=4*60*60*24*1000;
+            console.log('workdayMillies:', workdayMillies)
             // get timesheets entered within the last 3 business days
             timesheetList.map(sheet=>{
                 if (new Date().getTime() - new Date(sheet.timesubmitted).getTime() < workdayMillies) recentSheets.push(sheet);
@@ -23,6 +24,7 @@ const {Users} = require('../schemas/UserSchema');
             // check if active users have a timesheet in the last 3 business days
             let found;
             userList.map(async user=>{
+                console.log('1111111111111111111111111111111111')
                 found=false;
                 recentSheets.map(sheet=>{
                     if (sheet.userid === user.email) found=true;
@@ -30,6 +32,7 @@ const {Users} = require('../schemas/UserSchema');
                 // send email if one not sent within the last day
                 // if (!found&&user.reminderLastSent!==undefined&&(new Date().getTime())-(new Date(user.reminderLastSent).getTime())>=86400000) {
                 if (user.email==='tmurv@shaw.ca') {
+                    console.log('2222222222222222222222222222222222222222222')
                     if (timesheetReminder(user)) await Users.findOneAndUpdate({email: user.email}, {reminderLastSent: new Date()});
                 }
             });
