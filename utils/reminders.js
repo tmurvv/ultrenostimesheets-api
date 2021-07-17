39d331fc8c24f53491d9db66ce9f41f696a65cf9
tmurvv <tmurv@shaw.ca>
2021-07-17 12:51:25 -0600

    code cleanup

--- utils/reminders.js
+++ utils/reminders.js
@@ -6,18 +6,20 @@ function sendEmails() {
     const today = new Date();
     let userList;
     let timesheetList;
-    // Heroku Scheduler not running async await so promises used
+    // Heroku Scheduler not running async await portions of code so promises used instead
     if (today.getDay()>0&&today.getDay()<6) {
+        // Get Users
         Users
             .find()
             .then(function(items) {
                 userList=items;
                 const sheets = Timesheets.find()
+                // Get timesheets within last 3 business days
                 .then(function(timesheets){
                     timesheetList=timesheets;
                     // filter Timesheets for last three business days
                     const recentSheets = [];
-                    // adjust 3 days ago to 3 business days ago
+                    // adjust 3 days ago to 3 BUSINESS days ago
                     let workdayMillies = 3*60*60*24*1000;
                     if (new Date().getDay()===1) workdayMillies=5*60*60*24*1000;
                     if (new Date().getDay()===2) workdayMillies=4*60*60*24*1000;
@@ -32,12 +34,12 @@ function sendEmails() {
                         recentSheets.map(sheet=>{
                             if (sheet.userid === user.email) found=true;
                         });
-                        // send email if one not sent within the last day
+                        // if no recent timesheet, send email (if one not already sent within the last day)
                         if (!found&&user.reminderLastSent!==undefined&&(new Date().getTime())-(new Date(user.reminderLastSent).getTime())>=86400000) {
-                        // if (user.email==='tmurv@shaw.ca') {
                             if (timesheetReminder(user)) {
+                                // update user field lastReminderSent to today's date
                                 Users.findOneAndUpdate({email: user.email}, {reminderLastSent: new Date()})
-                                .then(console.log(`Success sending reminder email and updating reminderLastSent for ${user.email}`))
+                                .then(()=>console.log(`Success sending reminder email and updating reminderLastSent for ${user.email}`))
                                 .catch(()=>console.log(`Error updating reminderLastSent for ${user.email}`));
                             }
                         }
