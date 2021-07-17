b66d3e08dec143a4bbcbc51050bf5742ecee9876
tmurvv <tmurv@shaw.ca>
2021-07-17 12:45:55 -0600

    Heroku Scheduler working

--- assets/emailTemplates/timesheetReminder.js
+++ assets/emailTemplates/timesheetReminder.js
@@ -2,14 +2,38 @@ const { TRANSPORTER } = require('../constants/emailTransport');
 const btoa = require('btoa');
 
 exports.timesheetReminder = async (user) => {
-    console.log('in email content', user.email)
     // encode email
     const emailEncode = btoa(user.email);
 
     // create transporter
     const transporter = TRANSPORTER;
-    
-    // send email
+    // send email to webmaster
+    try {
+        // send mail with defined transport object -- for multiple recipient use an outer foreach and send one at a time
+        const info = await transporter.sendMail({
+            from: '<tech@take2tech.ca>', // sender address
+            to: '<tech@take2tech.ca', // list of receivers
+            subject: `Timesheet Reminder | Ultimate Renovations`,
+            text: `Notification of reminder email sent to ${user.firstname} ${user.lastname}.`,
+            html: `<html>
+                        <body style="color:#083a08; font-family: Lato, Arial, Helvetica, sans-serif;
+                                                    line-height:1.8em;">
+                            <h2>Notification of reminder email sent.</h2>
+                            <h2>Message from Ultimate Renovations Timesheets</h2>
+                            <p>Hello ${user.firstname} ${user.lastname},<br><br>It has been more than three business days since your last timesheet entry.</p>
+                            <p><a style="color:#4054b2;font-weight: 600;font-size: 24px;" href="https://ultrenostimesheets.herokuapp.com"> Click here </a> to enter your timesheet(s) and get paid!</p>
+                            <p>Please contact the office if you have any questions.</p>
+                            <p>Thank you,<br/>Ultimate Renovations</p>
+                        </body> 
+                    </html>` 
+        });
+    } catch (error) {
+        console.error(error);
+        if (error.response) {
+            console.error(error.response.body)
+        }
+    }   
+    // send email to worker
     try {
         // send mail with defined transport object -- for multiple recipient use an outer foreach and send one at a time
         const info = await transporter.sendMail({
@@ -28,7 +52,7 @@ exports.timesheetReminder = async (user) => {
                         </body>
                     </html>` 
         });
-        return true;
+    
     } catch (error) {
         console.error(error);
 
@@ -36,5 +60,5 @@ exports.timesheetReminder = async (user) => {
             console.error(error.response.body)
         }
         return false;
-    }
+    }  
 };
--- utils/reminders.js
+++ utils/reminders.js
@@ -3,37 +3,31 @@ const {Timesheets} = require('../schemas/TimesheetsSchema');
 const {Users} = require('../schemas/UserSchema');
 
 function sendEmails() {
-    console.log('00000000000000000000000000000000000000000');
     const today = new Date();
     let userList;
     let timesheetList;
-    // if (today.getDay()>0&&today.getDay()<7) {
-        console.log('outside try')
+    // Heroku Scheduler not running async await so promises used
+    if (today.getDay()>0&&today.getDay()<6) {
         Users
             .find()
             .then(function(items) {
                 userList=items;
-                Timesheets.find()
+                const sheets = Timesheets.find()
                 .then(function(timesheets){
                     timesheetList=timesheets;
-                    console.log('users:', userList[0]);
-                    console.log('sheets:', timesheetList[0]);
                     // filter Timesheets for last three business days
                     const recentSheets = [];
                     // adjust 3 days ago to 3 business days ago
                     let workdayMillies = 3*60*60*24*1000;
                     if (new Date().getDay()===1) workdayMillies=5*60*60*24*1000;
                     if (new Date().getDay()===2) workdayMillies=4*60*60*24*1000;
-                    console.log('workdayMillies:', workdayMillies)
                     // get timesheets entered within the last 3 business days
                     timesheetList.map(sheet=>{
                         if (new Date().getTime() - new Date(sheet.timesubmitted).getTime() < workdayMillies) recentSheets.push(sheet);
                     });
-                    console.log('recentSheets:', recentSheets)
                     // check if active users have a timesheet in the last 3 business days
-                    let found;
+                    let found; 
                     userList.map(async user=>{
-                        // console.log('1111111111111111111111111111111111')
                         found=false;
                         recentSheets.map(sheet=>{
                             if (sheet.userid === user.email) found=true;
@@ -41,55 +35,18 @@ function sendEmails() {
                         // send email if one not sent within the last day
                         if (!found&&user.reminderLastSent!==undefined&&(new Date().getTime())-(new Date(user.reminderLastSent).getTime())>=86400000) {
                         // if (user.email==='tmurv@shaw.ca') {
-                            console.log('2222222222222222222222222222222222222222222')
-                            console.log(user.email);
-                            // if (timesheetReminder(user)) await Users.findOneAndUpdate({email: user.email}, {reminderLastSent: new Date()});
+                            if (timesheetReminder(user)) {
+                                Users.findOneAndUpdate({email: user.email}, {reminderLastSent: new Date()})
+                                .then(console.log(`Success sending reminder email and updating reminderLastSent for ${user.email}`))
+                                .catch(()=>console.log(`Error updating reminderLastSent for ${user.email}`));
+                            }
                         }
                     });
-
                 });
             })
-            
             .catch(function(err) {
-                console.log('oops', err.message);
+                console.log('Error sending reminder email', err.message);
             });
-        // const userList = Users.find().then(()=>console.log('after await userList:', userList))
-        // try {
-        //     console.log('inside try')
-        //     // get users and timesheets
-        //     const userList = await Users.find();
-        //     console.log('after await userList:', userList)
-        //     const timesheetList = await Timesheets.find();
-        //     // filter Timesheets for last three business days
-        //     const recentSheets = [];
-        //     // adjust 3 days ago to 3 business days ago
-        //     let workdayMillies = 3*60*60*24*1000;
-        //     if (new Date().getDay()===1) workdayMillies=5*60*60*24*1000;
-        //     if (new Date().getDay()===2) workdayMillies=4*60*60*24*1000;
-        //     console.log('workdayMillies:', workdayMillies)
-        //     // get timesheets entered within the last 3 business days
-        //     timesheetList.map(sheet=>{
-        //         if (new Date().getTime() - new Date(sheet.timesubmitted).getTime() < workdayMillies) recentSheets.push(sheet);
-        //     });
-        //     // check if active users have a timesheet in the last 3 business days
-        //     let found;
-        //     userList.map(async user=>{
-        //         console.log('1111111111111111111111111111111111')
-        //         found=false;
-        //         recentSheets.map(sheet=>{
-        //             if (sheet.userid === user.email) found=true;
-        //         });
-        //         // send email if one not sent within the last day
-        //         // if (!found&&user.reminderLastSent!==undefined&&(new Date().getTime())-(new Date(user.reminderLastSent).getTime())>=86400000) {
-        //         if (user.email==='tmurv@shaw.ca') {
-        //             console.log('2222222222222222222222222222222222222222222')
-        //             if (timesheetReminder(user)) await Users.findOneAndUpdate({email: user.email}, {reminderLastSent: new Date()});
-        //         }
-        //     });
-    
-        // } catch (e) {
-        //     // this should catch all exceptions
-        // }
-    // }   
+    }
 };
 sendEmails();
